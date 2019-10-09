86dca18e5f6e77d10954db65f6339359bab71308
Chris Ahrens <christopher00ahrens@gmail.com>
2019-10-09 14:29:22 -0700

    MIPS Assembly updates
    exit code correction, register $v0 loaded with 10 and syscall

--- Lab_5/MyFirstMIPS.s
+++ Lab_5/MyFirstMIPS.s
@@ -1,5 +1,5 @@
 .data
-	# num:.word 0
+
 .text
 	
 	# lw $s0, num($0)
@@ -7,16 +7,19 @@
 	# move $t0, $s0
 	
 	# add $t0, $s0, $0
-	move $t0, $s0
+	
+	move $t0, $s0		# load s0 before this line executes
 	add $t1, $t0, 1
 	add $t2, $t1, 2
 	add $t3, $t2, 3
 	add $t4, $t3, 4
 	add $t5, $t4, 5
 	add $t6, $t5, 6
-	add $t7, $t6, 7
+	add $t7, $t6, 7		# addition using constant increments
 	
-	li $v0, 1
-	add $a0, $0, $t7
-	syscall
+	li $v0, 1 		# load v0 with instruction to print int
+	add $a0, $0, $t7	# load final number into a0 to be printed
+	syscall			# print a0 using v0 specification
+	li $v0, 10
+	syscall			# announces end of program?
 	
